fd6fe3fd5b1afbf9760d805a13cfae6c9b7f411a
boodizzle <john@titan-technology.com>
2017-01-18 19:17:54 -0800

    YAY

--- routes.js
+++ routes.js
@@ -7,19 +7,19 @@ module.exports = {
       api.authenticate(req.body, res);
     });
 
-    app.get('/auth/appt/', function (req, res) {
+    app.get('/auth/resource/appts', function (req, res) {
       api.getAppts(req.query, res);
     });
 
-    app.post('/auth/user/', function (req, res) {
+    app.post('/auth/newuser/', function (req, res) {
       api.createUser(req.body, res);
     });
 
-    app.put('/auth/user/', function (req, res) {
+    app.put('/auth/user/email', function (req, res) {
       api.updateEmail(req.body, res);
     });
 
-    app.delete('/auth/user/', function (req, res) {
+    app.delete('/auth/user/delete', function (req, res) {
       api.deleteUser(req.query, res);
     });
   }
